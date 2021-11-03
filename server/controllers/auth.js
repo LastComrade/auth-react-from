975444e6245a7b43b7938ba966f3c693c039d96c
Konark <konarklohat123456@gmail.com>
2021-11-03 16:09:33 +0530

    Controller => Auth => Login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const authCont = {
   register: async (req, res) => {
@@ -20,13 +21,50 @@ const authCont = {
           await newUser.save();
           return res.status(200).json({ message: "User created successfully" });
         }
-      }).clone().catch(function(err){ console.log(err)});
+      })
+        .clone()
+        .catch(function (err) {
+          console.log(err);
+        });
     } catch (err) {
       console.log(err);
     }
   },
-  login: (req, res) => {
-    return null;
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      await User.findOne({ email }, async (err, user) => {
+        if (err) {
+          return res
+            .status(500)
+            .json({ message: "Something went wrong. Please try again later" });
+        } else if (!user) {
+          return res.status(403).json({ message: "User not found" });
+        } else if (user) {
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (err) {
+              return res.status(500).json({ message: "Something went wrong" });
+            } else if (result) {
+              const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: "1h",
+              });
+              return res.status(200).json({
+                message: "Login successful",
+                token,
+              });
+            } else if (!result) {
+              return res.status(403).json({ message: "Incorrect password" });
+            }
+          });
+        }
+      })
+        .clone()
+        .catch(function (err) {
+          console.log(err);
+        });
+    } catch (err) {
+      console.log(err);
+    }
   },
   getUser: (req, res) => {
     return null;
